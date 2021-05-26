f852775621e37ad2006c2b8ee199c1906a4ec366
ttom795 <69662271+ttom795@users.noreply.github.com>
2021-05-26 14:13:29 +1200

    Update Chippure.py

--- Chippure.py
+++ Chippure.py
@@ -4,9 +4,6 @@ Chippure by Toby Tomkinson
 Copyright (c) 2021, Toby Tomkinson
 All rights reserved.
 
-This source code is licensed under the BSD-style license found in the
-LICENSE file in the root directory of this source tree.
-
 TODO:
 Sound toggle
 Speed adjustment
